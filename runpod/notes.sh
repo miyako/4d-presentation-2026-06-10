b63de4dcadedaeb5d3ca37d7ae66be19cf3a9271
miyako <keisuke.miyako@4D.com>
2026-05-27 07:24:02 +0900

    rm

--- runpod/notes.sh
+++
@@ -1,5 +0,0 @@
-#Resume from checkpoint:
-#torchrun --nproc_per_node=3 /workspace/train.py r10 true /workspace/bge_legal/r10/lora_adapter/checkpoint-5000 2>&1 | tee /workspace/train.log
-
-#Launch fresh:
-#torchrun --nproc_per_node=3 /workspace/train.py r10 false 2>&1 | tee /workspace/train.log
